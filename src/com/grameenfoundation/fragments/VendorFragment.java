f76b31c34efcf06da8788248f87f751f59c3d213
Ronald Kasagga <ronald.kasagga.k3@gmail.com>
2014-12-16 18:51:58 +0300

    made spinner display a list of market items

--- src/com/grameenfoundation/fragments/VendorFragment.java
+++ src/com/grameenfoundation/fragments/VendorFragment.java
@@ -1,20 +1,22 @@
 package com.grameenfoundation.fragments;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.*;
 import com.grameenfoundation.katale.R;
+import com.grameenfoundation.models.Market;
 import com.grameenfoundation.models.Vendor;
+
+import java.util.List;
 
 /**
  * Created by Ronald on 12/16/2014.
  */
-public class VendorFragment extends Fragment {
+public class VendorFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     private View view;
     private EditText vendorName;
@@ -27,7 +29,27 @@ public class VendorFragment extends Fragment {
         view = inflater.inflate(R.layout.vendor_form, null);
         vendorName = (EditText)view.findViewById(R.id.vendor_name_text_edit);
         marketSpinner = (Spinner)view.findViewById(R.id.market_spinner);
+        marketSpinner.setOnItemSelectedListener(this);
+        populateMarketSpinner();
         submitButton = (Button)view.findViewById(R.id.vendor_form_submit_button);
         return view;
+    }
+
+    public void populateMarketSpinner(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_spinner_dropdown_item, Market.getAllAsStringList());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        marketSpinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String item = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG ).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
--- src/com/grameenfoundation/models/Market.java
+++ src/com/grameenfoundation/models/Market.java
@@ -1,5 +1,8 @@
 package com.grameenfoundation.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ronald on 12/16/2014.
  */
@@ -11,6 +14,22 @@ public class Market {
         this.name = name;
     }
 
+    public static List<Market> getAll() {
+        List<Market> markets = new ArrayList<Market>();
+        markets.add(new Market("Kalerwe"));
+        markets.add(new Market("Owino"));
+        markets.add(new Market("Nakawa"));
+        markets.add(new Market("Bugolobi"));
+        return markets;
+    }
+
+    public static List<String> getAllAsStringList(){
+        List<String> markets = new ArrayList<String>();
+        for(Market market: getAll())
+            markets.add(market.name);
+        return markets;
+    }
+
     public String getName() {
         return name;
     }
@@ -18,5 +37,6 @@ public class Market {
     public void setName(String name) {
         this.name = name;
     }
+
 
 }
